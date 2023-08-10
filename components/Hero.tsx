3f495d23756783165d2bfd39dc71b7ab397d5d10
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-08-10 23:14:17 +0200

    Added Hero text layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="flex xl:flex-row flex-col gap-5 relative z-0 max-w-[1440px] mx-auto">
+      <div className="sm:px-16 px-6 flex-1 pt-36">
+        <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-extrabold">
+          JavCars - quickly and easily find and rent a car!
+        </h1>
+        <p className="text-[27px] text-black-100 font-light mt-5">
+          Drive premium, pay economy. Get affordable access to a wide selection
+          of premium car rentals.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
