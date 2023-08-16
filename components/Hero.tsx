939271cced88958e67c8ea773a87edf73ea98604
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-08-16 22:17:15 +0200

    Added hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,9 +22,14 @@ const Hero = () => {
       </div>
       <div className="xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
         <div className="relative xl:w-full w-[90%] xl:h-full h-[590px] z-0 drop-shadow-[0_25px_5px_rgba(0,0,0,0.25)]">
-          <Image src={'/hero.png'} alt="hero" fill className="object-contain" />
+          <Image
+            src={'/hero.png'}
+            alt="yellow car"
+            fill
+            className="object-contain"
+          />
         </div>
-        <div className="absolute xl:-top-24 xl:-right-1/2 -right-[5%] bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden" />
+        <div className="absolute xl:-top-24 xl:-right-1/2 -right-[25%] bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden" />
       </div>
     </div>
   );
