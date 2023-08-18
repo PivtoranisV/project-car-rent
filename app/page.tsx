0ec8bc0fff87503f7f189d21976627c37a8a9518
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-08-18 23:38:09 +0200

    Added car search layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import Filter from '@/components/Filter';
 import Hero from '@/components/Hero';
+import Search from '@/components/Search';
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 sm:px-16 px-6 py-4" id="catalogue">
+        <div className="flex flex-col items-start justify-start gap-y-2.5 text-black-100">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+        <div className="mt-12 w-full flex-between items-center flex-wrap gap-5">
+          <Search />
+        </div>
+        <div className="flex justify-start flex-wrap items-center gap-2">
+          <Filter />
+          <Filter />
+        </div>
+      </div>
     </main>
   );
 }
