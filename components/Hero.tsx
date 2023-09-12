7851776233f7f7692d7d6c9963b77d978145cfc5
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-09-13 00:18:36 +0300

    Fixed hero bg layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,15 +21,15 @@ const Hero = () => {
         />
       </div>
       <div className="xl:flex-[1.5] flex justify-end items-end w-full xl:h-screen">
-        <div className="relative xl:w-full w-[90%] xl:h-full h-[590px] z-0 drop-shadow-[0_25px_5px_rgba(0,0,0,0.25)]">
+        <div className="relative xl:w-full w-[90%] xl:h-full h-[350px] z-0 drop-shadow-[0_25px_5px_rgba(0,0,0,0.25)]">
           <Image
             src={'/hero.png'}
             alt="yellow car"
             fill
             className="object-contain"
           />
+          <div className="xl:-top-24 xl:-right-1/2 -right-1/4 bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[350px] overflow-hidden" />
         </div>
-        <div className="absolute xl:-top-24 xl:-right-1/2 -right-[25%] bg-hero-bg bg-repeat-round -z-10 w-full xl:h-screen h-[590px] overflow-hidden" />
       </div>
     </div>
   );
